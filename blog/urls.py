8d7263fa85c317ff3776911359da14b05d20406b
aryan <aryancomp0106@gmail.com>
2023-01-18 20:48:29 +0330

    create dynamic url for detail_page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listing_page/', views.listing_page, name='listing-page'),
-    path('detail_page/', views.detail_page, name='detail-page'),
+    path('', views.listing_page, name='listing_page'),
+    path('<int:pk>/', views.detail_page, name='detail_page'),
 ]
+
+# path('<int:pk>/', views.detail_page, name='detail_page'),
+# path('detail_page/', views.detail_page, name='detail_page'),
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from .models import Post
 
 
 def listing_page(request):
     posts_list = Post.objects.all()
-    return render(request, 'blog/listing-page.html', {'posts_list': posts_list})
+    return render(request, 'blog/listing_page.html', {'posts_list': posts_list})
 
 
-def detail_page(request):
-    return render(request, 'blog/detail-page.html')
+def detail_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/detail_page.html', {'post': post})
+
+
+
+
+    # print('ID IN URL:', pk)
+    # return HttpResponse(f'Id: {pk}')
+
+
+    # post = Post.objects.get(pk=pk)
+    # return render(request, 'blog/detail_page.html', {'post': post})
+
+
+    # return HttpResponse('hellooooooooooooooo')
+
+    # posts_list = Post.objects.all()
+    # return render(request, 'blog/detail_page.html', {'posts_list': posts_list})
+
+
